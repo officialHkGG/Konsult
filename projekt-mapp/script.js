8a1299c5b67b9c7ddb34c348ef060f7b65d090d2
Officialhk <Hidro.Kourkis@yh.nackademin.se>
2025-09-30 18:58:05 +0200

    first commit

---
+++ projekt-mapp/script.js
@@ -0,0 +1,5 @@
+// Enkel formulärhantering
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Tack för ditt meddelande! Vi återkommer inom 24 timmar.");
+});
